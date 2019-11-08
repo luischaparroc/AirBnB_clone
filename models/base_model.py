6d5631bb7362a595f2eb47c5494e6a70df07e570
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-11-08 15:13:39 +0000

    [update]: convert string to object created_at and updated_at Modified file: base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,19 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializes all attributes
         """
-        self.name = "Hello"
-        self.my_number = "45"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.name = ""
+            self.my_number = 0
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            self.name = kwargs['name']
+            self.my_number = kwargs['my_number']
+            self.id = kwargs['id']
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            self.created_at = datetime.strptime(kwargs['created_at'], f)
+            self.updated_at = datetime.strptime(kwargs['updated_at'], f)
 
     def __str__(self):
         """returns class name, id and attribute dictionary
