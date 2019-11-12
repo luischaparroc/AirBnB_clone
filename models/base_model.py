83e32e7054cea98ea937e666a18075d8d05c886e
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-11-12 15:31:19 +0000

    [fix] updated_at attribute modified file: base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
