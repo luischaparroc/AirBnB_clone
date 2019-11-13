cd4caa61eeb5dfc51ba6a7d7c3e5664c20369aa0
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-11-13 15:46:16 +0000

    [Fix] User files Modified files: user.py and test_user.py

--- models/user.py
+++ models/user.py
@@ -11,7 +11,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes attributes for User"""
-        super().__init__(**kwargs)
